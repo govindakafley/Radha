099e74c78289230923480c1fb925824ce106a622
unknown <govindakafley3@gmail.com>
2025-04-25 22:49:04 +0600

    added other additional features

---
+++ modules/automationandrobotics/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AutomationandroboticsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'automationandrobotics'
---
+++ modules/maintanences/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class MaintanencesConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'maintanences'
